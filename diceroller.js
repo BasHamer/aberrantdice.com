725a81a24b0a7e197117f20dd13b33c531e23f44
Bas Hamer <github@bashamer.com>
2021-01-15 09:04:25 -0700

    roll log working

--- diceroller.js
+++ diceroller.js
@@ -46,11 +46,11 @@ diceRoller.evaluateRoll = function(dataModel){
     if(dataModel.currentRoll.dice.find(notRevealed)){
         return;
     }
-    var botched = false;
+    var botch = false;
     var successes = 0;
     dataModel.currentRoll.dice.forEach(die=>{
-        if(die.isBotch && !botched){
-            botched = true;
+        if(die.isBotch){
+            botch = true;
         }
         if(die.isSuccess){
             if(die.isMega){
@@ -65,9 +65,15 @@ diceRoller.evaluateRoll = function(dataModel){
     });  
 
     dataModel.currentRoll.isSuccess = successes>=dataModel.currentRoll.rollConfig.difficulty;
-    dataModel.currentRoll.successes = successes;
-    dataModel.currentRoll.isBotch=(botched && dataModel.currentRoll.successes==0);
+    dataModel.currentRoll.successes = successes-dataModel.currentRoll.rollConfig.difficulty;
+    dataModel.currentRoll.isBotch=(botch && !dataModel.currentRoll.isSuccess);   
     dataModel.currentRoll.revealed = true;
+    dataModel.currentRoll.megaDice = _.filter(dataModel.currentRoll.dice, { 'isMega': true })
+                                        .map(x=>x.roll)
+                                        .reduce((x,y)=>x+", "+y);
+    dataModel.currentRoll.normalDice = _.filter(dataModel.currentRoll.dice, { 'isMega': false })
+                                        .map(x=>x.roll)
+                                        .reduce((x,y)=>x+", "+y);
 };
 
 diceRoller.addDie = function(dataModel, isMega){
@@ -174,18 +180,26 @@ diceRoller.init = function(config){
                         <div class="col-3">
                             <div class="container">
                                 <div class="row">
-                                    Pool 10/3
+                                    Pool {{roll.rollConfig.megaDicePool}}/{{roll.rollConfig.normalDicePool}}
                                 </div>
                                 <div class="row">
-                                    TN:7 Difficulty:1
+                                    TN:{{roll.rollConfig.targetNumber}} Difficulty:{{roll.rollConfig.difficulty}}
                                 </div>
                             </div>
                         </div>
                         <div class="col-6">
-                            1, 2, 3, 4, 5, 6, 7, 8, 9, <strong>10</strong> 
+                            <strong>{{roll.megaDice}}</strong> {{roll.normalDice}}
                         </div>
                         <div class="col-3">
-                            Success 
+                            <div class="result success" v-if="roll.isSuccess">
+                                <h2>Success</h2> with {{roll.successes}} extra successes
+                            </div>
+                            <div class="result botch" v-if="roll.isBotch">
+                                <h2>Botch</h2>
+                            </div>
+                            <div class="result" v-if="!roll.isBotch&&!roll.isSuccess">
+                                <h2>Failure</h2>
+                            </div>
                         </div>
                     </div>`
                     });
